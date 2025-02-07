d5ca49d095888ce4418f3f9c9c9bfd62fa610f1a
Nahom <8nahom8@gmail.com>
2025-02-07 13:42:37 -0600

    Finished main.py

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
+from mplsoccer import VerticalPitch
 import json
 import streamlit as st
 import pandas as pd
-import mplsoccer as mp
-from mplsoccer import VerticalPitchPitch
 
-st.title("Soccer Stats")
-st.header("Free Soccer data for Euros 2024")
 
-df = pd.read_csv("data\euros_2024_shot_map.csv")
+st.title("Euros 2024 Shot Map")
+st.subheader("Filter to any team/player to see all their shots taken.")
+
+df = pd.read_csv("C:\\Users\lilim\\OneDrive\\Desktop\\Streamlit\\data\\euros_2024_shot_map.csv")
 df = df[df['type'] == 'Shot'].reset_index(drop=True)
 df['location'] = df['location'].apply(json.loads)
 
-team = st.selectbox("Select a Team", df['team'].sort_values().unique(), index=None)
-player = st.selectbox("Select a Player", df['player'].sort_values().unique(), index=None)
+team = st.selectbox('Select a Team', df['team'].sort_values().unique(), index=None)
+player = st.selectbox('Select a Player', df[df['team'] == team]['player'].sort_values().unique(), index=None)
 
-def filteredData():
+def filterData(df, team, player):
     if team:
         df = df[df['team'] == team]
     if player:
-        df = df[df['player'] == team]
-    
-filtered_df = filteredData(df, team, player)
+        df = df[df['player'] == player]
 
-pitch = VerticalPitchPitch(pitch_type='statsbomb', half = True)
-fix, ax = pitch.draw(figsize=(10,10))
+    return df
 
-def PlotShos(df, ax, pitch):
+filtered_df = filterData(df, team, player)
+
+pitch = VerticalPitch(pitch_type="statsbomb", half=True)
+fig, ax = pitch.draw(figsize=(10,10))
+
+def plotShots(df, ax, pitch):
     for x in df.to_dict(orient='records'):
-        pitch.scatter(x=float['location'][0]), y=float['location'[1]],ax=ax, s=1000 * x['shots_statsbomb_xg'], color = 'green'
+        pitch.scatter(
+            x=float(x['location'][0]),  # X location (horizontal)
+            y=float(x['location'][1]),  # Y location (vertical)
+            ax=ax,
+            s=1000 * x["shot_statsbomb_xg"],  # Bubble size based on expected goal value
+            color='green' if x['shot_outcome'] == 'Goal' else 'white',
+            edgecolors='black',
+            alpha=1 if x['shot_outcome'] == 'Goal' else 0.5,
+            zorder=2 if x['shot_outcome'] == 'Goal' else 1
+        )
+plotShots(filtered_df, ax, pitch)
 
-        if x['shot_outcome']
+st.pyplot(fig)
