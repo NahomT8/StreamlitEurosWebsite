afbd519fb19b19f7531abf7abfa3063a013a89d4
Nahom <8nahom8@gmail.com>
2025-02-17 09:53:49 -0600

    Changed test.py code so it now graphs whatever the matchid was and it saves it automatically on the scrape.py script. Huge update. I plan to make it more interactive

--- scrape.py
+++ scrape.py
@@ -3,21 +3,35 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 def scrapeData():
-    matchid = input("Enter a match id ")
+    matchid = input("Enter a match id: ")
 
-    response = requests.get("https://understat.com/match/"+matchid)
-    print(response.status_code)
+    response = requests.get(f"https://understat.com/match/{matchid}")
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+        return
 
     soup = BeautifulSoup(response.content, 'html.parser')
     ugly_soup = str(soup)
 
-    shotsData = re.search("var shotsData .*= JSON.parse\('(.*)'\)", ugly_soup).group(1)
-    data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
+    match = re.search(r"var shotsData .*= JSON.parse\('(.*)'\)", ugly_soup)
+    if not match:
+        print("Could not find shot data.")
+        return
 
-    shotsDf = pd.DataFrame(data['h']) #the h means home, which is Liverpool in my case :)
-    print(shotsDf.head())
+    shotsData = match.group(1)
+    data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
+    shotsDf = pd.DataFrame(data['h'])
+    filename = f"match_{matchid}_shots.csv"
+    os.makedirs("data", exist_ok=True)
+    filepath = os.path.join("data", filename)
+    shotsDf.to_csv(filepath, index=False)
+    print(f"Data saved to {filepath}")
+
+    return matchid
 
 if __name__ == "__main__":
     scrapeData()
--- test.py
+++ test.py
@@ -2,12 +2,18 @@ from mplsoccer import *
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+from scrape import scrapeData
 
-data = pd.read_csv("data/match_26797_shots.csv")
+matchid = scrapeData()
+
+if matchid:
+    data_path = (f'data/match_{matchid}_shots.csv')
+    data = pd.read_csv(data_path)
+
 
 pitch = Pitch()
 fig, ax = pitch.draw(figsize=(8, 4))
 
-ax.text(pitch.dim.length / 2, pitch.dim.width + 0.1, 'Liverpool shots against Manchester United', ha='center', va='top', fontsize=16, color='black')
+ax.text(pitch.dim.length / 2, pitch.dim.width + 0.1, 'Shot Map', ha='center', va='top', fontsize=16, color='black')
 ax.scatter(data['X'] * pitch.dim.length, data['Y'] * pitch.dim.width, s=100, c='#ea6969', alpha=0.7)
 plt.show()
