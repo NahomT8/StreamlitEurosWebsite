bdcf4ddd015d8438604c1756196946d18c8099f8
Nahom <8nahom8@gmail.com>
2025-02-16 20:00:59 -0600

    rewrote the code so now it graphs the shots with red dots on the pitch

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 from mplsoccer import *
-import streamlit
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data\euros_2024_shot_map.csv")
-data = data[data['type'] == "Shot"].reset_index(drop=True)
-data['location'] = data['location'].apply(json.loads)
-
+data = pd.read_csv("data/match_26797_shots.csv")
 
 pitch = Pitch()
 fig, ax = pitch.draw(figsize=(8, 4))
+
+ax.text(pitch.dim.length / 2, pitch.dim.width + 0.1, 'Liverpool shots against Manchester United', ha='center', va='top', fontsize=16, color='black')
+ax.scatter(data['X'] * pitch.dim.length, data['Y'] * pitch.dim.width, s=100, c='#ea6969', alpha=0.7)
 plt.show()
