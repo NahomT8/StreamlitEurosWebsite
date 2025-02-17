4826f0b5a929bca820592435b5e396692683a130
Nahom <8nahom8@gmail.com>
2025-02-17 10:11:17 -0600

    changed test.py script

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from mplsoccer import *
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 from scrape import scrapeData
 
 matchid = scrapeData()
@@ -12,8 +13,11 @@ if matchid:
 
 
 pitch = Pitch()
+pitch_length = pitch.dim.length
+pitch_width = pitch.dim.width
 fig, ax = pitch.draw(figsize=(8, 4))
 
-ax.text(pitch.dim.length / 2, pitch.dim.width + 0.1, 'Shot Map', ha='center', va='top', fontsize=16, color='black')
-ax.scatter(data['X'] * pitch.dim.length, data['Y'] * pitch.dim.width, s=100, c='#ea6969', alpha=0.7)
+
+ax.text(pitch_length / 2, pitch_width + 15, 'Shot Map', ha='center', va='top', fontsize=16, color='black')
+ax.scatter(data['X'] * pitch_length, data['Y'] * pitch_width, s=100, c='#ea6969', alpha=0.7)
 plt.show()
