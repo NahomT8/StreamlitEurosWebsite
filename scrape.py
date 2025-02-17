a02c4523fc4879e41f04989bd63daf36cb9e1ed6
Nahom <8nahom8@gmail.com>
2025-02-17 09:25:43 -0600

    changed code so it is in a function and will only ask for matchid if the function is called instead of automatically asking for matchid

--- scrape.py
+++ scrape.py
@@ -4,16 +4,20 @@ import json
 from bs4 import BeautifulSoup
 import pandas as pd
 
-matchid = input("Enter a match id ")
+def scrapeData():
+    matchid = input("Enter a match id ")
 
-response = requests.get("https://understat.com/match/"+matchid)
-print(response.status_code)
+    response = requests.get("https://understat.com/match/"+matchid)
+    print(response.status_code)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-ugly_soup = str(soup)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    ugly_soup = str(soup)
 
-shotsData = re.search("var shotsData .*= JSON.parse\('(.*)'\)", ugly_soup).group(1)
-data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
+    shotsData = re.search("var shotsData .*= JSON.parse\('(.*)'\)", ugly_soup).group(1)
+    data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
 
-shotsDf = pd.DataFrame(data['h']) #the h means home, which is Liverpool in my case :)
-print(shotsDf.head())
+    shotsDf = pd.DataFrame(data['h']) #the h means home, which is Liverpool in my case :)
+    print(shotsDf.head())
+
+if __name__ == "__main__":
+    scrapeData()
