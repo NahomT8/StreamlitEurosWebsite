ec2c729fe5e3f865791bdc4f75d9cf74d95436bd
Nahom <8nahom8@gmail.com>
2025-02-16 18:39:28 -0600

    updated script to save data in a df

--- scrape.py
+++ scrape.py
@@ -2,6 +2,18 @@ import re
 import requests
 import json
 from bs4 import BeautifulSoup
+import pandas as pd
 
-response = requests.get("https://understat.com/match/26797")
+matchid = input("Enter a match id ")
+
+response = requests.get("https://understat.com/match/"+matchid)
 print(response.status_code)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+ugly_soup = str(soup)
+
+shotsData = re.search("var shotsData .*= JSON.parse\('(.*)'\)", ugly_soup).group(1)
+data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
+
+shotsDf = pd.DataFrame(data['h']) #the h means home, which is Liverpool in my case :)
+print(shotsDf.head())
